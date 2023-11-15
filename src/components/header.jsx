13fc3bf35d82036f41874080f2531193bdbf1f1c
dillonpw <dillonpwalsh10@gmail.com>
2023-11-15 14:27:01 -0500

    padding and responsiveness

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -24,25 +24,23 @@ export default function Header() {
           </a>
         </div>
       </div>
-      <div className="column flex items-center justify-center pt-10">
-        <div className="flex w-1/2 flex-col p-6">
-          <h1 id="top" className="pb-2 text-5xl">
+      <div className="flex flex-col items-center justify-center pt-10 md:flex-row">
+        <div className="flex w-full flex-col px-6 md:w-1/2">
+          <h1 id="top" className="pb-2 px-5 text-5xl">
             Dillon Walsh
           </h1>
-          <h2 className="text-2xl">Full Stack Web Developer and Designer</h2>
-          <div className="text-2xl">
-            <p>Thanks for stopping by!</p>
-          </div>
-          <p className="p-5">Catchy paragraph.</p>
+          <h2 className="pb-2 px-5 text-2xl">
+            Full Stack Web Developer and Designer
+          </h2>
         </div>
-        <div className="mt-10 flex w-1/2 p-6">
-          <p>
+        <div className="mt-5 flex w-full px-6 md:w-1/2">
+          <p className="px-6">
             After graduating from University in 2019, I took a full time
-            posiiton as a 911 emergency dispatcher. Though the job could be
+            position as a 911 emergency dispatcher. Though the job could be
             demanding, it also afforded me plenty of downtime which I used to
             learn web development using free resources like freeCodeCamp and The
-            Odin Project. Since then I have been designing, building, consulting
-            and contiuning to learn. Now I contribute to The Odin Project
+            Odin Project. Since then I have been designing, building
+            and continuing to learn. Now I contribute to The Odin Project
             curriculum and build projects for myself and customers in my spare
             time.
           </p>
