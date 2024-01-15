f85ec6d125d9e3ccafb6cb26bf19e80bd48c885e
dillonpw <dillonpwalsh10@gmail.com>
2024-01-15 10:46:39 -0500

    updated contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,28 @@ import useScrollIn from './ScrollIn'
 import { useState } from 'react'
 
 const Contact = () => {
+  const [result, setResult] = useState()
   const [ref, isVisible] = useScrollIn()
-  const [submitState, setSubmitState] = useState(false)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setSubmitState(true)
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    setResult('Sending....')
+    const formData = new FormData(event.target)
+
+    formData.append('access_key', 'a1bf7f2c-e2c4-4575-9d2e-ac544037c627')
+
+    const res = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    }).then((res) => res.json())
+
+    if (res.success) {
+      console.log('Success', res)
+      setResult(res.message)
+    } else {
+      console.log('Error', res)
+      setResult(res.message)
+    }
   }
 
   return (
@@ -15,55 +31,42 @@ const Contact = () => {
       ref={ref}
       className={`scroll-in ${
         isVisible ? 'visible' : ''
-      } my-6 border-cyan-200 p-10`}
+      } m-8 border-cyan-200 md:max-w-[700px]`}
     >
-      {!submitState ? (
-        <form
-          className="flex flex-col w-full"
-          action="https://api.web3forms.com/submit"
-          method="POST"
-          onSubmit={handleSubmit}
+      <h1 className="text-center text-3xl">Want to get in touch?</h1>
+      <form className="flex flex-col" onSubmit={onSubmit}>
+        <label className="font-bold" htmlFor="email">
+          Email:
+        </label>
+        <input
+          className="my-2 h-10 w-[50%] rounded-md p-2"
+          type="email"
+          name="email"
+          id="email"
+          required
+          placeholder="Email@address.com"
+        />
+
+        <label className="font-bold" htmlFor="message">
+          Message:
+        </label>
+        <textarea
+          className="my-2 w-[70%] rounded-lg p-2"
+          name="message"
+          id="message"
+          rows="6"
+          required
+          placeholder="Your message..."
+        ></textarea>
+
+        <button
+          className="w-fit items-center justify-center rounded-lg bg-cyan-200 px-8 py-4 text-sky-950 opacity-90 hover:scale-105 hover:opacity-100"
+          type="submit"
         >
-          <p className="mb-3 text-3xl">Want to get in touch?</p>
-          <input
-            type="hidden"
-            name="access_key"
-            value="a1bf7f2c-e2c4-4575-9d2e-ac544037c627"
-          />
-          <label className="font-bold" htmlFor="email">
-            Email:{' '}
-          </label>
-          <input
-            className="my-2 h-10 max-w-[400px] rounded-md p-2"
-            type="email"
-            name="email"
-            id="email"
-            required
-            placeholder="Email@address.com"
-          />
-          <label className="font-bold" htmlFor="message">
-            Message:
-          </label>
-          <textarea
-            className="text-sky-950 my-2 max-w-[400px] rounded-lg p-2"
-            name="message"
-            rows="6"
-            required
-            placeholder="..."
-          ></textarea>
-          <div className="h-captcha my-2" data-captcha="true"></div>
-          <button
-            className="w-fit items-center justify-center rounded-lg bg-cyan-200 p-4 px-8 text-sky-950 opacity-90  hover:scale-105 hover:opacity-100"
-            type="submit"
-          >
-            Send
-          </button>
-        </form>
-      ) : (
-        <p text-center text-3xl>
-          Thanks for reaching out! I'll get back to you as soon as I can!
-        </p>
-      )}
+          Submit
+        </button>
+      </form>
+      <span className="text-center">{result}</span>
     </div>
   )
 }
