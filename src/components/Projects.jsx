6ef5cb5cb72325a3af95b88bf37052d5c9a60509
dillonpw <dillonpwalsh10@gmail.com>
2024-01-22 13:30:09 -0500

    aspect ratio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
               to={`/projects/${image.id}`}
             >
               <img
-                className="aspect-auto h-[280px] w-[480px] rounded-2xl border-2 border-white"
+                className="aspect-auto h-[360px] w-[480px] rounded-2xl border-2 border-white"
                 src={image.src}
                 alt={image.title}
               />
