496a480857c4650271791c92b4c92cbd0e5538e7
dillonpw <dillonpwalsh10@gmail.com>
2024-01-22 13:26:22 -0500

    image tweak

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
               to={`/projects/${image.id}`}
             >
               <img
-                className="h-[277px] w-[480px] rounded-2xl border-2 border-white"
+                className="h-[280px] w-[480px] rounded-2xl border-2 border-white"
                 src={image.src}
                 alt={image.title}
               />
