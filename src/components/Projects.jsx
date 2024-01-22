363347497d5fd5740fefd944310ea5ac1eb898a1
dillonpw <dillonpwalsh10@gmail.com>
2024-01-22 13:14:20 -0500

    adjusted sizes of images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ const Projects = () => {
               to={`/projects/${image.id}`}
             >
               <img
-                className="h-auto aspect-auto w-auto rounded-2xl border-2 border-white"
+                className="aspect-auto h-auto w-auto rounded-2xl border-2 border-white"
                 src={image.src}
                 alt={image.title}
               />
