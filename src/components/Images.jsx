fe927458f872e08cf09038248dd18482b9e184ee
dillonpw <dillonpwalsh10@gmail.com>
2024-01-27 16:46:23 -0500

    re-ordered images

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,5 +1,19 @@
 const images = [
   {
+    id: 'ecommerce-site',
+    src: '/assets/ecommerce.png',
+    mobileImg: '/assets/ecommerce-m.png',
+    livePreviewLink: 'https://shop-red-eta.vercel.app/',
+    githubLink: 'https://github.com/Dillonpw/ecommerce',
+    title: 'Ecommerce Site',
+    tags: ['TypeScript', 'React', 'Tailwind', 'TailwindUI', 'Shadcn/ui'],
+    challenge:
+      'This project was rather large and with that came some challenges: for one I struggled a bit with sharing state between components and pages, I failed to realize that the concept I was inventing in my head and almost in my code already existed in the form of useContex... Once that was out of the way I found some difficulty in positioniing certain elements exactly where I wanted them so they looked presentable both on desktop and mobile but a few tailwind size adjustements sorted that out.',
+    description:
+      'This template is part of The Odin Project React curriculum You will notce a checkout area if you decide to go through te motions of the website, it does not submit anything it, it clears the cart. Engaging in this project introduced me to many new concepts within React. I dove headfirst into the useContext hook and scoured the React documentation to gain what I believe is a working grasp of its uses. In the end, I am happy with the outcome. The goal was to create something purposely generic and minimal, and I believe that’s what I have achieved.',
+  },
+
+  {
     id: 'dispatcher-cheat-sheet',
     src: '/assets/DCS-home.png',
     mobileImg: '/assets/DCS-home-m.png',
@@ -13,19 +27,7 @@ const images = [
       "A live and public site with returning users, styled with a brutaslist theme in mind. Consisting of a few useful tools for 911 dispatchers, a job I worked for several years. I built the app to help with my efficiency as a dispatcher and as a send off gift to my former coworkers. The primary funciton is a work schedule generator. As a 911 dispatcher, my schedule was different every week due to the need for 24/7 uptime, and to prevent the same poeple from always working weekends we worked on a rotation of 4 work days followed by 3 days off, which required a great deal of effort to schedule anything in advance. I plan to add more tools to the page as they come to mind and offer this project up to other 911 dispatchers to hopefully make their jobs and lives easier as well.",
   },
 
-  {
-    id: 'ecommerce-site',
-    src: '/assets/ecommerce.png',
-    mobileImg: '/assets/ecommerce-m.png',
-    livePreviewLink: 'https://shop-red-eta.vercel.app/',
-    githubLink: 'https://github.com/Dillonpw/ecommerce',
-    title: 'Ecommerce Site',
-    tags: ['TypeScript', 'React', 'Tailwind', 'TailwindUI', 'Shadcn/ui'],
-    challenge:
-      'This project was rather large and with that came some challenges: for one I struggled a bit with sharing state between components and pages, I failed to realize that the concept I was inventing in my head and almost in my code already existed in the form of useContex... Once that was out of the way I found some difficulty in positioniing certain elements exactly where I wanted them so they looked presentable both on desktop and mobile but a few tailwind size adjustements sorted that out.',
-    description:
-      'This template is part of The Odin Project React curriculum You will notce a checkout area if you decide to go through te motions of the website, it does not submit anything it, it clears the cart. Engaging in this project introduced me to many new concepts within React. I dove headfirst into the useContext hook and scoured the React documentation to gain what I believe is a working grasp of its uses. In the end, I am happy with the outcome. The goal was to create something purposely generic and minimal, and I believe that’s what I have achieved.',
-  },
+
 
   {
     id: 'cv-template',
