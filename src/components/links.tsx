f8de36290156b33b4ecc2b9a0d0d778adadb542b
dillonpw <dillonpwalsh10@gmail.com>
2025-03-08 09:18:50 -0500

    referrer

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -39,7 +39,7 @@ export default function Links() {
           <a
             href={link.href}
             target={link.isExternal ? "_blank" : "_self"}
-            rel={link.isExternal ? "noopener noreferrer" : undefined}
+            rel={link.isExternal ? "noopener referrer" : undefined}
           >
             <Button
               className="w-22 bg-emerald-500 text-center text-sm font-semibold text-gray-950 hover:bg-emerald-400 md:text-base dark:bg-emerald-500/80 dark:hover:bg-emerald-400"
